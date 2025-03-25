fa880db4ff828941a86c0cb96f19a17d7080405f
akihiro <okitwi1000@gmail.com>
2025-03-25 23:21:02 +0900

    bugfix

--- copy_gh_parmalink/src/extension.ts
+++ copy_gh_parmalink/src/extension.ts
@@ -4,7 +4,7 @@ import * as path from "path";
 
 export function activate(context: vscode.ExtensionContext) {
   let disposable = vscode.commands.registerCommand(
-    "extension.copyGitHubPermalink",
+    "extension.copyGitHubPermalinkShort",
     async () => {
       const editor = vscode.window.activeTextEditor;
       if (!editor) {
